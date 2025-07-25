ba7edaa508e46dd600f307678437572e6968b566
mikewguo <mikeguot@gtinternational.org>
2025-07-24 18:28:18 -0700

    run.sh to use localhost

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ done
 # Example environment variables for config (adjust as needed)
 export JICOFO_CONFIG_DIR="$BASEDIR/resources/config"
 export LOGGING_CONFIG="$BASEDIR/lib/logging.properties"
-CONFIG_FILE="$BASEDIR/jicofo-selector/src/main/resources/reference.conf"
+CONFIG_FILE="$BASEDIR/jicofo-selector/src/main/resources/localhost.reference.conf"
 
 # Run the main class
 exec java -cp "$CLASSPATH:$JICOFO_JAR" -Djava.util.logging.config.file="$LOGGING_CONFIG" -Dconfig.file="$CONFIG_FILE" org.jitsi.jicofo.Main "$@" 
